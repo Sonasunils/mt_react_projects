e2c950977415c95c2d75c2178bc1555d1df5bba2
Sonasunils <sona2002sunil@gmail.com>
2024-07-15 15:45:04 +0530

    About props, props types , Default props

---
+++ props-in-react/src/App.jsx
@@ -0,0 +1,16 @@
+import Student from './Student'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Student name = "sona" age = {30} isStudent={true}/>
+    <Student name = "Anju" age = {12} isStudent={true}/>
+    <Student/>
+    </>
+  )
+}
+
+export default App
