348b7bd1c20e73887fa1644e5abf3530244ff09d
Sonasunils <sona2002sunil@gmail.com>
2024-07-15 13:25:39 +0530

    react sample card

---
+++ react-card/card/src/Card.jsx
@@ -0,0 +1,13 @@
+function Card()
+{
+    return(
+      <div className="myCard">
+        <img className='card-image'src="https://www.shutterstock.com/image-photo/young-happy-latin-woman-using-260nw-2277463121.jpg" alt="" />
+        <h1>Sona</h1>
+        <h2>Python full stack developer</h2>
+      </div>
+   
+    );
+
+}
+export default Card
